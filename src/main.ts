c86e7785fd4876a384f99a3c2c0315b2e414eba8
Haroun <harounakinda.pro@gmail.com>
2026-02-01 17:14:54 +0000

    Cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
     bodyParser: false,
   });
 
+  app.enableCors({
+    origin: "*", // ⚠️ à restreindre en prod
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false, // true seulement si cookies
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
